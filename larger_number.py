4eb9623fa3e30b6d13c838add66f4fdd0d8cf54f
EDWINE MICHEL <Edwine@EDWINEs-MacBook-Air-2.local>
2017-08-17 22:14:29 -0600

    Done

---
+++ larger_number.py
@@ -0,0 +1,6 @@
+x = int(input("enter x:"))
+y = int(input("enter y:"))
+if x > y:
+    print("x is larger")
+if x < y:
+    print('y is larger')
---
+++ neg_pos_zero.py
@@ -0,0 +1,7 @@
+my_num = int(input('Enter a number:'))
+if my_num > 0:
+    print("The inputted number is positive")
+elif my_num == 0:
+    print("The inputted number is zero")
+else:
+    print("The inputted number is negative")
